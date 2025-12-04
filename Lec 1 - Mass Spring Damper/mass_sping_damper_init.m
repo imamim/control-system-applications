60b7a848c36fd3d62ab852ce3f26f3805374c7f7
hasanpul <hasanpul04@gmail.com>
2025-12-04 19:23:19 +0300

    PID subsystemine low-pass eklendi. I_value, control_output vs. lazım olan grafikler beraber görüntülenebiliyor. PID bloğu ve subsysteminin karşılaştırması görülebiliyor.

--- Lec 1 - Mass Spring Damper/mass_sping_damper_init.m
+++ Lec 1 - Mass Spring Damper/mass_sping_damper_init.m
@@ -1,5 +1,10 @@
 msd_param = struct();
 
-msd_param.k_spring = 20;
-msd_param.b_damper = 10;
+msd_param.k_spring = 1;
+msd_param.b_damper = 1;
 msd_param.m_kg     = 1;
+
+Kp = 2.0878469575047;
+Ki = 1.1899891295634;
+Kd = 0.909872512100581;
+N = 182.0881725666;
